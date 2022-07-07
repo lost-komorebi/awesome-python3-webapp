d5ec17f212229c4f1c79cebe56757b4f7eb90be8
komorebi <10271080@qq.com>
2022-07-07 15:14:47 +0700

    漏掉了括号

--- www/models.py
+++ www/models.py
@@ -43,7 +43,7 @@ class Blog(Model):
 class Comment(Model):
     __table__ = 'comments'
 
-    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+    id = StringField(primary_key=True, default=next_id(), ddl='varchar(50)')
     blog_id = StringField(ddl='varchar(50)')
     user_id = StringField(ddl='varchar(50)')
     user_name = StringField(ddl='varchar(50)')
