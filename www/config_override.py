a1315938aa700db1d3e571cfd80b76df9c71bc32
komorebi <10271080@qq.com>
2022-07-13 13:32:10 +0700

    添加线上db信息

--- www/config_override.py
+++ www/config_override.py
@@ -6,5 +6,9 @@ __author__ = 'komorebi'
 """用于线上的配置文件，用来覆盖某些配置"""
 
 configs = {
-    'db': {"host": '127.0.0.1'}
+    'db': {
+        "host": '127.0.0.1',
+        "user": 'awesome',
+        "password": 'password'
+    }
 }
